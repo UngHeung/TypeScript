8a05a9d0b37b03f62255d920860f3dc16efa6d13
ungHeung <t01047377477@gmail.com>
2023-07-04 14:09:49 +0900

    Feat : boolean 값을 통해 값의 출력 제어

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function add(num1, num2) {
-    return num1 + num2;
+function add(num1, num2, isTrue) {
+    if (isTrue) {
+        console.log(num1 + num2);
+    }
+    else {
+        return num1 + num2;
+    }
 }
 var num1 = 10;
 var num2 = 5.5;
-var result = add(num1, num2);
-console.log(result);
+var isConsole = false;
+var result = add(num1, num2, isConsole);
--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
-function add(num1: number, num2: number) {
-    return num1 + num2;
+function add(num1: number, num2: number, isTrue: boolean) {
+    if (isTrue) {
+        console.log(num1 + num2);
+    } else {
+        return num1 + num2;
+    }
 }
 
 const num1 = 10;
 const num2 = 5.5;
-const result = add(num1, num2);
-
-console.log(result);
+const isConsole = false;
+const result = add(num1, num2, isConsole);
