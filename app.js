953c7d90c1fc4e30aa8729b6a867fa85e82e46d1
ungHeung <t01047377477@gmail.com>
2023-07-04 13:41:10 +0900

    Fix : 정해진 타입이 입력되도록 수정

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function add(num1, num2) {
     return num1 + num2;
 }
-var num1 = "10";
+var num1 = 10;
 var num2 = 5.5;
 var result = add(num1, num2);
 console.log(result);
--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ function add(num1: number, num2: number) {
     return num1 + num2;
 }
 
-const num1 = "10";
+const num1 = 10;
 const num2 = 5.5;
 const result = add(num1, num2);
 
