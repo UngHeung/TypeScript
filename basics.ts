ad34656e77de2b86d87e0e547e5f557afed6ea11
ungHeung <t01047377477@gmail.com>
2023-07-04 14:42:43 +0900

    Rename : 객체로 넘어가기 위해 기존 app.ts파일 이름 변경

---
+++ basics.ts
@@ -0,0 +1,16 @@
+function add(num1: number, num2: number, isTrue: boolean, str1: string) {
+    const result = num1 + num2;
+    if (isTrue) {
+        console.log(str1 + result);
+    } else {
+        return num1 + num2;
+    }
+}
+
+const num1 = 10;
+const num2 = 5.5;
+let num3;
+num3 = "1";
+const isConsole = true;
+const str1 = "결과 : ";
+const result = add(num1, num2, isConsole, str1);
