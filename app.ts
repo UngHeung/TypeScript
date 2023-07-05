04c0f606dfb0ee6cc63e9c276e8211d89c67759a
ungHeung <t01047377477@gmail.com>
2023-07-05 15:37:54 +0900

    Feat : 타입스크립트 배열 타입과 잘못된 할당 예시

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-const fruit: {
-    name: string;
-    count: number;
-} = {
+// 객체
+
+const fruit = {
     name: "Apple",
     count: 5,
 };
 
 console.log(fruit.name);
+
+// 배열
+
+let fruits: string[];
+fruits = "orange"; // error
+fruits = ["orange", 1]; // error
+fruits = ["orange", "melon"];
