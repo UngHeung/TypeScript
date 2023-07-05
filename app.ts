fcd147e57eacf9dc18f0167f2a1e745c5d585d7b
ungHeung <t01047377477@gmail.com>
2023-07-05 16:06:44 +0900

    Feat : 배열에 들어갈 요소의 타입을 할당해주면 좋은 이유

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@
 const fruit = {
     name: "Apple",
     count: 5,
+    colors: ["red", "green"],
 };
 
 console.log(fruit.name);
@@ -13,3 +14,8 @@ let fruits: string[];
 fruits = "orange"; // error
 fruits = ["orange", 1]; // error
 fruits = ["orange", "melon"];
+
+for (const color of fruit.colors) {
+    console.log(color.length);
+    color.map(() => {}); // error
+}
