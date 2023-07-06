8f941ff8a8b275d45a25ec6389fac5ef7dd40d95
ungHeung <t01047377477@gmail.com>
2023-07-06 23:01:46 +0900

    Feat : 유니언 타입 연산 에러 해결

--- app.js
+++ app.js
@@ -1,46 +1,16 @@
-// 객체
-// const fruit: {
-//     name: string;
-//     count: number;
-//     colors: string[];
-//     role: [number, string];
-// } = {
-//     name: "Apple",
-//     count: 5,
-//     colors: ["red", "green"],
-//     role: [2, "author"],
-// };
-// const role: [number, string] = [1, "man"];
-// const role2 = [2, "woman"];
-// role[1] = 11;
-// role2[1] = 22;
-// let role3: [number, string];
-// role3 = [3, "new man", "after 2000"];
-// console.log(fruit.name);
-// // 배열
-// let fruits: string[];
-// fruits = "orange"; // error
-// fruits = ["orange", 1]; // error
-// fruits = ["orange", "melon"];
-// for (const color of fruit.colors) {
-//     console.log(color.length);
-//     color.map(() => {}); // error
-// }
-// enum 열거형
-var Human;
-(function (Human) {
-    Human["HEAD"] = "\uBA38\uB9AC";
-    Human["BODY"] = "\uBAB8";
-    Human["ARMS"] = "\uD314";
-    Human["LEGS"] = "\uB2E4\uB9AC";
-})(Human || (Human = {}));
-var eye = {
-    position: Human.HEAD,
-};
-function getPosition(part) {
-    if (part.position === Human.HEAD) {
-        return part.position;
+// union
+// 숫자, 혹은 문자열을 처리할 수 있는 함수
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
     }
-    return undefined;
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-console.log(getPosition(eye));
+var combineNumber = combine(10, 20);
+var combineString = combine("wel", "come");
+console.log(combineNumber);
+console.log(combineString);
--- app.ts
+++ app.ts
@@ -2,9 +2,19 @@
 // 숫자, 혹은 문자열을 처리할 수 있는 함수
 
 function combine(input1: number | string, input2: number | string) {
-    const result = input1 + input2;
+    let result;
+
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+
     return result;
 }
 
 const combineNumber = combine(10, 20);
 const combineString = combine("wel", "come");
+
+console.log(combineNumber);
+console.log(combineString);
