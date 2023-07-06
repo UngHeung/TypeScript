20e0c5f40ad3e6d7e3439560fd74b5f5b9aa9ad8
ungHeung <t01047377477@gmail.com>
2023-07-06 22:11:05 +0900

    Feat : enum

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-var fruit = {
-    name: "Apple",
-    count: 5,
+// 객체
+// const fruit: {
+//     name: string;
+//     count: number;
+//     colors: string[];
+//     role: [number, string];
+// } = {
+//     name: "Apple",
+//     count: 5,
+//     colors: ["red", "green"],
+//     role: [2, "author"],
+// };
+// const role: [number, string] = [1, "man"];
+// const role2 = [2, "woman"];
+// role[1] = 11;
+// role2[1] = 22;
+// let role3: [number, string];
+// role3 = [3, "new man", "after 2000"];
+// console.log(fruit.name);
+// // 배열
+// let fruits: string[];
+// fruits = "orange"; // error
+// fruits = ["orange", 1]; // error
+// fruits = ["orange", "melon"];
+// for (const color of fruit.colors) {
+//     console.log(color.length);
+//     color.map(() => {}); // error
+// }
+// enum 열거형
+var eye = {
+    position: "head",
 };
-console.log(fruit.name);
+function getPosition(part) {
+    if (part.position === "머리") {
+        return part.position;
+    }
+    return undefined;
+}
+console.log(getPosition(eye));
--- app.ts
+++ app.ts
@@ -1,36 +1,51 @@
 // 객체
 
-const fruit: {
-    name: string;
-    count: number;
-    colors: string[];
-    role: [number, string];
-} = {
-    name: "Apple",
-    count: 5,
-    colors: ["red", "green"],
-    role: [2, "author"],
+// const fruit: {
+//     name: string;
+//     count: number;
+//     colors: string[];
+//     role: [number, string];
+// } = {
+//     name: "Apple",
+//     count: 5,
+//     colors: ["red", "green"],
+//     role: [2, "author"],
+// };
+
+// const role: [number, string] = [1, "man"];
+// const role2 = [2, "woman"];
+
+// role[1] = 11;
+// role2[1] = 22;
+
+// let role3: [number, string];
+// role3 = [3, "new man", "after 2000"];
+
+// console.log(fruit.name);
+
+// // 배열
+
+// let fruits: string[];
+// fruits = "orange"; // error
+// fruits = ["orange", 1]; // error
+// fruits = ["orange", "melon"];
+
+// for (const color of fruit.colors) {
+//     console.log(color.length);
+//     color.map(() => {}); // error
+// }
+
+// enum 열거형
+
+const eye = {
+    position: "head",
 };
 
-const role: [number, string] = [1, "man"];
-const role2 = [2, "woman"];
-
-role[1] = 11;
-role2[1] = 22;
-
-let role3: [number, string];
-role3 = [3, "new man", "after 2000"];
-
-console.log(fruit.name);
-
-// 배열
-
-let fruits: string[];
-fruits = "orange"; // error
-fruits = ["orange", 1]; // error
-fruits = ["orange", "melon"];
-
-for (const color of fruit.colors) {
-    console.log(color.length);
-    color.map(() => {}); // error
+function getPosition(part) {
+    if (part.position === "머리") {
+        return part.position;
+    }
+    return undefined;
 }
+
+console.log(getPosition(eye));
