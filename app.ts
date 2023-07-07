fd909b6511014bbd466cddc640b3da79ef950cf4
ungHeung <t01047377477@gmail.com>
2023-07-07 18:07:53 +0900

    Feat : 반환 값이 없는 함수의 반환 타입

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
-function printString(str: string): string {
+function printString(str: string) {
     return str;
 }
 
 function pringNumber(num: number): string {
     return num.toString();
+}
+
+function nonPrintValue(val: any) {
+    console.log(val);
 }
