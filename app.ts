af00c75cc5d85dce70350d1181bc9a2bc09c614f
ungHeung <t01047377477@gmail.com>
2023-07-13 13:22:33 +0900

    Feat : 에러 없이 unkown 타입을 다른 타입의 변수에 할당하기

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 // 어떤 타입을 할당할지 알 수 없음
-let userInput: any;
+let userInput: unknown;
 let userName: string;
 
-userInput = 1;
 userInput = "1";
+userInput = 1;
 
-userName = userInput;
+if (typeof userInput === "string") {
+    userName = userInput;
+}
