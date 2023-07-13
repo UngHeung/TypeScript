f20d7aad92eb6288d0ffb64da74692741532da42
ungHeung <t01047377477@gmail.com>
2023-07-13 13:40:52 +0900

    Feat : 아무것도 반환하지 않는 함수의 타입

--- app.ts
+++ app.ts
@@ -8,3 +8,9 @@ userInput = 1;
 if (typeof userInput === "string") {
     userName = userInput;
 }
+
+function generateError(message: string, code: number): void {
+    throw { message: message, errorCode: code };
+}
+
+generateError("에러 발생", 500);
