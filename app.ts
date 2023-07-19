3cd931bc0eef8ff4c9b12b7fe745a33170ee875b
ungHeung <t01047377477@gmail.com>
2023-07-19 19:22:37 +0900

    Chore : 다음 학습을 위한 파일 이동

--- app.ts
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector("button")!;
-button.addEventListener("click", () => {
-  console.log("클릭!");
-});
-
-// lib이 활성되었지만 값이 비어있으므로 TS는 document와 console을 제대로 인식하지 못한다.
