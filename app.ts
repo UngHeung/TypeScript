6e05fdf8ecdf7ca1c7aa728efd4e283614293f69
ungHeung <t01047377477@gmail.com>
2023-07-26 18:19:35 +0900

    Generic - union 타입 사용하기

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@
  * Generic
  */
 
-function add(val1: unknown, val2: unknown) {
+function add(val1: string | number, val2: string | number) {
   if (typeof val1 === "number" && typeof val2 === "number") {
     return val1 + val2;
   } else if (val1 === "string" && typeof val2 === "string") {
@@ -10,6 +10,6 @@ function add(val1: unknown, val2: unknown) {
   }
 }
 
-// unknown 타입은 모든 타입을 사용할 수 있지만
-// 타입을 엄격하게 검사하기 때문에 조건문을 사용하여 타입을 확인해주어야 한다.
+// union 타입을 사용할 수 있지만
+// unknown과 같은 이유로 조건문을 사용하여 타입을 확인해주어야 한다.
 // number + string과 같은 교차 연산이 되지 않도록 하기 위함
