4a7e7daf538ca5f00f14500cc287efd0c75fee89
ungHeung <t01047377477@gmail.com>
2023-07-26 18:21:43 +0900

    Generic - 함수 오버로드 사용하기

--- app.ts
+++ app.ts
@@ -2,14 +2,10 @@
  * Generic
  */
 
-function add(val1: string | number, val2: string | number) {
-  if (typeof val1 === "number" && typeof val2 === "number") {
-    return val1 + val2;
-  } else if (val1 === "string" && typeof val2 === "string") {
-    return val1 + val2;
-  }
+function add(val1: string, val2: string): string;
+function add(val1: number, val2: number): number;
+function add(val1: any, val2: any) {
+  return val1 + val2;
 }
 
-// union 타입을 사용할 수 있지만
-// unknown과 같은 이유로 조건문을 사용하여 타입을 확인해주어야 한다.
-// number + string과 같은 교차 연산이 되지 않도록 하기 위함
+// 함수 오버로드를 사용할 수 있다.
