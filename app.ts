8304ab4cbe1d73f383c1456ec5efb21c5111c3a6
ungHeung <t01047377477@gmail.com>
2023-07-26 18:14:49 +0900

    Generic - any 타입 사용하기

--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@
  * Generic
  */
 
-function add(val1, val2) {
+function add(val1: any, val2: any) {
   return val1 + val2;
 }
+
+// any 타입은 모든 타입을 사용할 수 있지만
+// 타입을 검사하지 않으므로, 타입스크립트를 사용하는 이유를 무색하게 한다.
