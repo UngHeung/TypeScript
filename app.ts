ae2783e17fdb83b97f95b15fb828fcc32115f167
ungHeung <t01047377477@gmail.com>
2023-07-26 18:18:06 +0900

    Generic - unknown 타입 사용하기

--- app.ts
+++ app.ts
@@ -2,9 +2,14 @@
  * Generic
  */
 
-function add(val1: any, val2: any) {
-  return val1 + val2;
+function add(val1: unknown, val2: unknown) {
+  if (typeof val1 === "number" && typeof val2 === "number") {
+    return val1 + val2;
+  } else if (val1 === "string" && typeof val2 === "string") {
+    return val1 + val2;
+  }
 }
 
-// any 타입은 모든 타입을 사용할 수 있지만
-// 타입을 검사하지 않으므로, 타입스크립트를 사용하는 이유를 무색하게 한다.
+// unknown 타입은 모든 타입을 사용할 수 있지만
+// 타입을 엄격하게 검사하기 때문에 조건문을 사용하여 타입을 확인해주어야 한다.
+// number + string과 같은 교차 연산이 되지 않도록 하기 위함
