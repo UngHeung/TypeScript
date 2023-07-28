db6fbe6d74b27ee213a9f0dd3a54fd0b50237b69
ungHeung <t01047377477@gmail.com>
2023-07-28 16:43:00 +0900

    Generic - 타입 명시

--- app.ts
+++ app.ts
@@ -6,17 +6,17 @@ function newArray<T>(element1: T, element2: T): T[] {
   return [element1, element2];
 }
 
-newArray(1, 2);
+newArray<number>(1, 2);
 newArray("a", 2);
-newArray("a", "b");
+newArray<string>("a", "b");
 newArray(true, 1);
-newArray(false, true);
+newArray<boolean>(false, true);
 
 function newArray2<T, T2>(element: T, element2: T2) {
   return [element, element2];
 }
 
-newArray2(1, 2);
-newArray2(1, "a");
-newArray2(true, "b");
-newArray2("a", "b");
+newArray2<number, number>(1, 2);
+newArray2<number, string>(1, "a");
+newArray2<boolean, string>(true, "b");
+newArray2<string, string>("a", "b");
