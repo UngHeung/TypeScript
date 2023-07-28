806e99bd3fe7da8b579674f916c0aacfa33bff8d
ungHeung <t01047377477@gmail.com>
2023-07-28 17:19:36 +0900

    Generic - 객체 비교하기

--- app.ts
+++ app.ts
@@ -13,3 +13,15 @@ function getLength<T extends restriction>(value: T): number {
 getLength("hello");
 getLength(10); // Error
 getLength(true); // Error
+
+// 객체 비교하기
+function getProperty<T, O extends keyof T>(obj: T, key: O) {
+  return obj[key];
+}
+
+let obj = { a: 1, b: 2, c: 3 };
+let obj2 = { d: 4, e: 5, f: 6 };
+
+getProperty(obj, "a");
+getProperty(obj2, "d");
+getProperty(obj, "b");
