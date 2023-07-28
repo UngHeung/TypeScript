0fcfe145be5a1b4f885fddd4bec8ad59c2c11456
ungHeung <t01047377477@gmail.com>
2023-07-28 17:06:45 +0900

    Generic - 제약 조건의 사용 방법

--- app.ts
+++ app.ts
@@ -2,6 +2,14 @@
  * Generic
  */
 
-function getLength<T>(value: T): number {
+interface restriction {
+  length: number;
+}
+
+function getLength<T extends restriction>(value: T): number {
   return value.length;
 }
+
+getLength("hello");
+getLength(10); // Error
+getLength(true); // Error
