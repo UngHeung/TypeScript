eb1978fcdeeef36ad0d9b43a4bcb9b38e0a89763
ungHeung <t01047377477@gmail.com>
2023-07-28 17:03:05 +0900

    Generic - 제약 조건

--- app.ts
+++ app.ts
@@ -2,21 +2,6 @@
  * Generic
  */
 
-function newArray<T>(element1: T, element2: T): T[] {
-  return [element1, element2];
+function getLength<T>(value: T): number {
+  return value.length;
 }
-
-newArray<number>(1, 2);
-newArray("a", 2);
-newArray<string>("a", "b");
-newArray(true, 1);
-newArray<boolean>(false, true);
-
-function newArray2<T, T2>(element: T, element2: T2) {
-  return [element, element2];
-}
-
-newArray2<number, number>(1, 2);
-newArray2<number, string>(1, "a");
-newArray2<boolean, string>(true, "b");
-newArray2<string, string>("a", "b");
